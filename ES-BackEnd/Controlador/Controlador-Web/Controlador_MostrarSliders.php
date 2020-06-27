0d1d82eb1943f320f62c18e57e90146ddae46a7c
Angy <angychapaprincipe@gmail.com>
2020-06-27 10:56:12 -0500

    VISTA COMPONENTE 1 DEL CMS

---
+++ ES-BackEnd/Controlador/Controlador-Web/Controlador_MostrarSliders.php
@@ -0,0 +1,29 @@
+<?php
+
+//
+//LLAMADA A LOS ARCHIVOS DE CONEXION A LA BD Y EL MODELO CORRESPONDIENTE AL CONTROLADOR
+//
+//METODO GET
+
+require_once(__DIR__."/../../Modelo/ConexionBD.php");
+require_once(__DIR__."/../../Modelo/Modelo_Web.php");
+
+$Model_Web = new Model_Web();
+
+if($Model_Web->mostrarSliders()){                        
+    
+    $array = $Model_Web->mostrarSliders();  
+
+}else{                        
+    $array = array(
+        "response" => 0,
+        "message" => "Informacion no encontrada"                    
+        );                        
+}
+
+header('Content-type: application/json; charset=utf-8');
+
+echo json_encode($array);   
+
+?>
+
